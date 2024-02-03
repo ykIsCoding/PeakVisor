4537c712b80fdd67fc8193ec3f1cf03c2d12203a
ykIsCoding <Gamerlykesdiary@gmail.com>
2024-02-03 19:06:55 +0800

    added frontend

---
+++ frontend/peakvisor/src/app/pages/error404page/error404page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error404page',
+  standalone: true,
+  imports: [],
+  templateUrl: './error404page.component.html',
+  styleUrl: './error404page.component.css'
+})
+export class Error404pageComponent {
+
+}
---
+++ frontend/peakvisor/src/app/pages/signuppage/signuppage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signuppage',
+  standalone: true,
+  imports: [],
+  templateUrl: './signuppage.component.html',
+  styleUrl: './signuppage.component.css'
+})
+export class SignuppageComponent {
+
+}
