fb0dd784871bff0a691a0667e6ee2e8c021588b7
ykIsCoding <Gamerlykesdiary@gmail.com>
2024-02-05 11:34:15 +0800

    added home

--- frontend/peakvisor/src/app/app.component.ts
+++ frontend/peakvisor/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { provideLottieOptions } from 'ngx-lottie';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLinkActive,RouterLink],
+  imports: [RouterOutlet, RouterLinkActive,RouterLink, HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
