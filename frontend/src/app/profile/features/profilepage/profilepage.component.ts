03a2b0ae6b690b5008c7184d5dad108ff9eb1fc5
malepsyche <114489469+malepsyche@users.noreply.github.com>
2024-03-19 09:32:39 +0800

    Added strava service and injected http module into main.ts

--- frontend/src/app/profile/features/profilepage/profilepage.component.ts
+++ frontend/src/app/profile/features/profilepage/profilepage.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+
 import { ProfilestatsComponent } from '@app/profile/ui/profilestats/profilestats.component';
 import { ButtonComponent } from '@app/shared/ui/button/button.component';
 import { GridsectionComponent } from '@app/shared/ui/gridsection/gridsection.component';
 import { HeaderComponent } from '@app/shared/ui/header/header.component';
+
+import { StravaAuthService } from '@app/services/strava/strava-auth.service';
 
 @Component({
   selector: 'app-profilepage',
@@ -13,4 +17,17 @@ import { HeaderComponent } from '@app/shared/ui/header/header.component';
 })
 export class ProfilepageComponent {
   content = [{name:'June 10 Hike'},{name:'June 20 Hike'},{name:'June 30 Hike'},]
+
+  constructor(
+    private stravaAuthService: StravaAuthService,
+    private location: Location,
+  ){
+
+  }
+  
+  requestStravaAuth(): void {
+    const currentUrl: string = window.location.href;
+    this.stravaAuthService.redirectToStravaAuth(currentUrl);
+  }
+
 }
